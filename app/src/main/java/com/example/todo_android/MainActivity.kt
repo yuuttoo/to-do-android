bffb9b538877f0197c52aca957bbd3ebc0604138
Cheng-Yu Chung <chengyu.chung@gosaico.com>
2024-10-18 13:53:01 +0800

    Added bottom bar

--- app/src/main/java/com/example/todo_android/MainActivity.kt
+++ app/src/main/java/com/example/todo_android/MainActivity.kt
@@ -6,6 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,9 +29,37 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Todo_androidTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//
-//                }
+                Scaffold(
+                    bottomBar = {
+                        BottomAppBar(
+                            actions = {
+                                IconButton(onClick = { /* do something */ }) {
+                                    Icon(Icons.Filled.Create, contentDescription = "Localized description")
+                                }
+                                IconButton(onClick = { /* do something */ }) {
+                                    Icon(
+                                        Icons.Filled.Check,
+                                        contentDescription = "Localized description",
+                                    )
+                                }
+                            },
+                            floatingActionButton = {
+                                FloatingActionButton(
+                                    onClick = { /* do something */ },
+                                    containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+                                    elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
+                                ) {
+                                    Icon(Icons.Filled.Add, "Localized description")
+                                }
+                            }
+                        )
+                    },
+                ) { innerPadding ->
+                    Text(
+                        modifier = Modifier.padding(innerPadding),
+                        text = "Create to do list here"
+                    )
+                }
             }
         }
     }
